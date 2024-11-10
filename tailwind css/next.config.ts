82788e10dba7a1b2a1bab3e6b30788154a31a3df
Aliyan <aliyan.aq718@gmail.com>
2024-11-10 20:12:41 +0500

    new commit

--- tailwind css/next.config.ts
+++ tailwind css/next.config.ts
@@ -2,7 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // Add any custom serverless function or export here
+  webpack(config) {
+    // Custom Webpack configuration for CSS
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
