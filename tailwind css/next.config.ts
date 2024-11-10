dbe7ed8f0876374c4223191acc7fdf1fc1221fda
Aliyan <aliyan.aq718@gmail.com>
2024-11-10 20:01:41 +0500

    new commit

--- tailwind css/next.config.ts
+++ tailwind css/next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  // Add any custom serverless function or export here
 };
 
 export default nextConfig;
